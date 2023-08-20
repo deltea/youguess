20b5356fc20aa23274ee80f75117f6cada7e00f2
Leo <thcheetah777@gmail.com>
2023-08-20 17:33:05 +0300

    add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,12 @@
 </svelte:head>
 
 <script lang="ts">
+  import { dev } from "$app/environment";
+  import { inject } from "@vercel/analytics";
+
   import "../global.css";
+
+  inject({ mode: dev ? "development" : "production" });
 </script>
 
 <div class="h-screen">
