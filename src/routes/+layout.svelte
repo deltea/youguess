0ba87f1f666a318fee1b8497100b1bf72546e47b
Leo <thcheetah777@gmail.com>
2023-08-21 19:37:07 +0300

    add even more meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,15 @@
   <meta name="description" content="Which YouTube video has more views?">
   <meta name="keywords" content="YouTube, Guessing Game">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <meta property="og:title" content="YouGuess">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://youguess.vercel.app">
+  <meta property="og:image" content="/logo.png">
+  <meta property="og:description" content="Which YouTube video has more views?">
+  <meta name="theme-color" content="#FF0000">
+
+  <meta name="twitter:card" content="summary_large_image">
 </svelte:head>
 
 <script lang="ts">
